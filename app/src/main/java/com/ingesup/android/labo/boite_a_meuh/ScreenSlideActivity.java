be58a2dd0dcecab900f153a3c720ec24aa0c63d3
MatheaR <mathea.renaudie@gmail.com>
2014-11-05 17:06:47 +0100

    Avancement slides

--- app/src/main/java/com/ingesup/android/labo/boite_a_meuh/ScreenSlideActivity.java
+++ app/src/main/java/com/ingesup/android/labo/boite_a_meuh/ScreenSlideActivity.java
@@ -8,21 +8,79 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.app.FragmentManager;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 /**
  * Created by Mathea on 15/10/2014.
  */
-public class ScreenSlideActivity extends FragmentActivity {
-    /**
-     * The pager widget, which handles animation and allows swiping horizontally to access previous
-     * and next wizard steps.
-     */
+
+    public class ScreenSlideActivity extends Activity implements View.OnTouchListener {
+    ImageView img;
+    float x1,x2;
+    float y1, y2;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meuh);
+
+        img = (ImageView) findViewById(R.id.imageView);
+        img.setOnTouchListener(this);
+    }
+
+    public boolean onTouch(View v, MotionEvent touchevent) {
+        switch (touchevent.getAction())
+        {
+            // when user first touches the screen we get x and y coordinate
+            case MotionEvent.ACTION_DOWN:
+            {
+                x1 = touchevent.getX();
+                y1 = touchevent.getY();
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+            {
+                x2 = touchevent.getX();
+                y2 = touchevent.getY();
+
+                //if left to right sweep event on screen
+                if (x1 < x2)
+                {
+                    Toast.makeText(this, "Left to Right Swap Performed", Toast.LENGTH_LONG).show();
+                }
+
+                // if right to left sweep event on screen
+                if (x1 > x2)
+                {
+                    Toast.makeText(this, "Right to Left Swap Performed", Toast.LENGTH_LONG).show();
+                }
+
+                // if UP to Down sweep event on screen
+                if (y1 < y2)
+                {
+                    Toast.makeText(this, "UP to Down Swap Performed", Toast.LENGTH_LONG).show();
+                }
+
+                //if Down to UP sweep event on screen
+                if (y1 > y2)
+                {
+                    Toast.makeText(this, "Down to UP Swap Performed", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+        return false;
+    }
+}
+
+/* public class ScreenSlideActivity extends FragmentActivity {
+
     private ViewPager mPager;
-    /**
-     * The pager adapter, which provides the pages to the view pager widget.
-     */
+
     private PagerAdapter mPagerAdapter;
 
     @Override
@@ -51,10 +109,6 @@ public class ScreenSlideActivity extends FragmentActivity {
         }
     }
 
-    /**
-     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
-     * sequence.
-     */
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
 
         private ArrayList<SlideFragment> liste;
@@ -74,4 +128,4 @@ public class ScreenSlideActivity extends FragmentActivity {
             return liste.size();
         }
     }
-}
+}*/
